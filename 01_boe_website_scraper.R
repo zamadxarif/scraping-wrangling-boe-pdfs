1ca17275a4a96cc35b9c6254b02a58000b3f341f
Zahid Amadxarif <zahid.amadxarif@outlook.com>
2021-03-02 10:19:26 +0000

    Update 01_boe_website_scraper.R

--- 01_boe_website_scraper.R
+++ 01_boe_website_scraper.R
@@ -9,7 +9,7 @@ page <- read_html("https://www.bankofengland.co.uk/sitemap/prudential-regulation
 
 ## Gather vector of urls on the page for every document
 
-## extract links for each release (SS - supervisory-statement; PS - policy-statement; SoP - statement-of-policy)
+## extract links for each release (SS - supervisory-statement, PS - policy-statement, SoP - statement-of-policy)
 SS_release_home_url <- page %>% ## find all urls associated with the supervisory-statement subheading
   html_nodes(".list-links__item:nth-child(25) .list-links__item .icon-download") %>% 
   html_attr('href')
@@ -20,7 +20,8 @@ SoP_release_home_url <- page %>% ## find all urls associated with the statement-
   html_nodes(".list-links__item:nth-child(21) .list-links__item .icon-download") %>% 
   html_attr('href')
 
-## extract text associated with each link (SS - supervisory-statement; PS - policy-statement)
+## extract text associated with each link (SS - supervisory-statement, PS - policy-statement, SoP - statement-of-policy)
+
 SS_release_title <- page %>% 
   html_nodes(".list-links__item:nth-child(25) .list-links__item .icon-download") %>% 
   html_text() %>% stri_trim() %>% stri_sub(1, -12) %>% stri_trim() %>% ## Extract text and remove (pdf 12mb) text from end
